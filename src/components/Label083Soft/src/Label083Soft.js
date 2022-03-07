f8ade33968831b53c091f9088db389e24801605e
github083 <083softemail@gmail.com>
2022-03-07 12:40:36 -0500

    feat(labeo083soft): crate package

---
+++ src/components/Label083Soft/src/Label083Soft.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function Label083Soft(props) {
+    return <p>mi label</p>
+}
+
+export default Label083Soft
---
+++ src/components/Label083Soft/src/index.js
@@ -0,0 +1,3 @@
+import Label083Soft from './Label083Soft'
+
+export default Label083Soft
